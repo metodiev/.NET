b97f1345d65577ad870b219aa467797840e50257
metodiev <miroslav.l.metodiev@gmail.com>
2019-01-06 20:53:14 +0200

    push to master

---
+++ CsharpIntro/MathematicalOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntro
+{
+    class MathematicalOperations
+    {
+        public static void Main(string[] args)
+        {
+            //this is comment in C#
+            /***
+             * Write a program to sum two numbers
+             */
+            Console.WriteLine("Enter number one:");
+            int numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number two:");
+            int numberTwo = int.Parse(Console.ReadLine());
+            //generate new method with ctrl and dot shorcut
+            Console.WriteLine(sumTwoNumbers(numberOne, numberTwo) );
+
+            //Home work write logics with another operations
+            //use methods
+        }
+
+        
+        private static int sumTwoNumbers(int numberOne, int numberTwo)
+        {
+            if ((numberOne < 0 || numberTwo < 0) )
+            {
+                Console.WriteLine("Invalid input");
+                return 0;
+            }
+            return numberTwo + numberOne;
+        }
+    }
+
+  
+}
---
+++ CsharpIntro/Statments.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntro
+{
+    class Statments
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your current age");
+            int age = int.Parse(Console.ReadLine());
+
+            checkUserAge(age);
+
+            switch (checkUserAge(age))
+            {
+                case true:
+                    printAllowSiteMessage();
+                    break;
+
+                case false:
+                    printWarningSiteMessage();
+                    break;
+
+                default:
+                    break;
+            }
+
+            //switch statment choice number
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Two");
+                   // Console.ReadLine();
+                    break;
+
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+
+                case 3:
+                 
+
+                case 4:
+                    Console.WriteLine("Four");
+                    Console.ReadLine();
+                    break;
+      
+            }
+
+           //Home work write a coupe if and switch statments
+
+        }
+
+        //warning site message
+        private static void printWarningSiteMessage()
+        {
+            Console.WriteLine("Warning you dont have enougn age to be here please visit www.cartoonnetwork.com");
+        }
+
+        //sucessfull site message
+        private static void printAllowSiteMessage()
+        {
+            Console.WriteLine("Sucessfull login");
+        }
+
+        //check current age if the user
+        private static bool checkUserAge(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
