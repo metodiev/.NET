10e729115243a7f66558051dc306eeef2c9b8961
metodiev <miroslav.l.metodiev@gmail.com>
2019-01-07 23:30:50 +0200

    Commit Loops examples C#

--- CsharpIntro/Loops.cs
+++ CsharpIntro/Loops.cs
@@ -10,7 +10,41 @@ namespace CSharpIntro
     {
         public static void Main(string [] args)
         {
+            Console.WriteLine("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            forLoop(number);
+            whileCycle(number);
+            doWhileCycle(number);
+            Console.ReadLine();
 
+            //Home work reverse all cycles
+            
+        }
+
+        private static void doWhileCycle(int number)
+        {
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number > 0);
+        }
+
+        private static void whileCycle(int number)
+        {
+            while (number > 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+        }
+
+        private static void forLoop(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(i);
+            }
         }
     }
 }
