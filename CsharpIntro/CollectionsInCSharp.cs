d50fa194e57d0a122ad702a69656ecbe3ca696b7
metodiev <miroslav.l.metodiev@gmail.com>
2019-01-29 12:05:37 -0800

    Reverse string example and float and double examp
    Reverse string example and float and double examp

---
+++ CsharpIntro/CollectionsInCSharp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class CollectionsInCSharp
+    {
+        static void Main(string[] args)
+        {
+            //float numbers
+            int peshoApples = 416516515;
+            float devideApples= (float)peshoApples / (float)10.0000000;
+            float floatNumber = 101.415151f;
+            Console.WriteLine(devideApples);
+            float pi = 3.14f; // - the precision is 7 digits
+            Console.WriteLine(pi);
+            ////the precision is 14 digits
+            double doubleNumber = 12.4151616516516511;
+            Console.WriteLine(doubleNumber);
+
+            Console.WriteLine("kiro e {1} zashtoto ima {0}", floatNumber, doubleNumber);
+
+            //List
+            int[] numbers = new int[100];
+
+            var list = new List<String>();
+            list.Add("Str");
+
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add("Test: " + i);
+            }
+
+            foreach (string item in list)
+            {
+                //Console.WriteLine(item);
+            }
+            //  int p = 10;
+
+         
+            //var list1 = CreateList(o, o1);
+
+            //var listGenerics = new List<>();
+
+            Console.ReadLine();  
+        }
+
+        public static List<T> CreateList<T>(params T[] elements)
+        {
+            return new List<T>(elements);
+        }
+
+    }
+}
+
---
+++ CsharpIntro/ReverseString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class ReverseString
+    {
+        public static void Main(string [] args)
+        {
+            //Learning Java
+            string [] input = Console.ReadLine().Split(' ');
+            
+            Stack<string> stack = new Stack<string>();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            int stackSize = stack.Count();
+            for (int i = 0; i < stackSize; i++)
+            {
+                Console.Write(reverseWord(stack.Pop()));
+                Console.Write(" ");
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        private static string reverseWord(string v)
+        {
+            char[] arr = new char[v.Length];
+            int counter = 0;
+            for (int i = v.Length-1; i >= 0; i--)
+            {
+                arr[counter] = v[i];
+                counter++;
+            }
+
+            string retrunStr = new string(arr);
+            return retrunStr;
+        }
+    }
+}
